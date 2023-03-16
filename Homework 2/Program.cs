2078c253b0d044429bbe9f968251b50124fc45ee
bojanGulicoski1 <119902954+bojanGulicoski1@users.noreply.github.com>
2023-03-16 15:47:42 +0100

    homework1
    homework1

---
+++ Homework 2/Program.cs
@@ -0,0 +1,55 @@
+﻿//Swap Numbers  - Task One
+
+int number1, number2;
+Console.Write("Input the First Number : ");
+number1 = int.Parse(Console.ReadLine());
+Console.Write("Input the Second Number : ");
+number2 = int.Parse(Console.ReadLine());
+(number1, number2) = (number2, number1);
+Console.Write("After Swapping : ");
+Console.Write("\nFirst Number : " + number1);
+Console.Write("\nSecond Number : " + number2);
+
+//SumOfEven - Task Two
+
+int[] array = new int[6];
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine($"Enter integer {i + 1}: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int sumOfEven = 0;
+foreach (int num in array)
+{
+    if (num % 2 == 0)
+    {
+        sumOfEven += num;
+    }
+}
+Console.WriteLine($"The sum of even numbers is {sumOfEven}");
+
+
+//Student group 
+
+string[] studentsG1 = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+string[] studentsG2 = { "Igor", "Bojan", "Darko", "Zivko", "Ivan" };
+
+Console.WriteLine("Enter Student group:(There are 1 and 2)");
+
+int userChoice  = Convert.ToInt32(Console.ReadLine());
+
+if (userChoice == 1)
+{
+    foreach (string student in studentsG1)
+    {
+        Console.WriteLine(student);
+    }
+}else if (userChoice == 2)
+{
+    foreach (string student in studentsG2)
+    {
+        Console.WriteLine(student);
+    }
+}
+
---
+++ Homework1/Program.cs
@@ -0,0 +1,65 @@
+﻿//// See https://aka.ms/new-console-template for more information
+
+////Task One- Real Calculator
+//Console.WriteLine("Enter First Number:");
+//int input1 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Enter Second Number:");
+//int input2 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Enter Operator:");
+//string inputOperator = Console.ReadLine();
+//if (inputOperator == "+")
+//{
+//    Console.WriteLine(input1 + input2);
+//}
+//if (inputOperator == "-")
+//{
+//    Console.WriteLine(input1 - input2);
+//}
+//if (inputOperator == "*")
+//{
+//    Console.WriteLine(input1 * input2);
+//}
+//if (inputOperator == "/")
+//{
+//    Console.WriteLine(input1 / input2);
+//}
+
+//Task Two - Average numbers
+
+int input3 = Convert.ToInt32(Console.ReadLine());
+int input4 = Convert.ToInt32(Console.ReadLine());
+int input5 = Convert.ToInt32(Console.ReadLine());
+int input6 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine((input3 + input4 + input5 + input6) / 4);
+
+//Task one with switch
+//Real calculator
+Console.WriteLine("Please enter number one");
+int userNum1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter number two");
+int userNum2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter operator");
+char operatorInput = Convert.ToChar(Console.ReadLine());
+
+switch (operatorInput)
+{
+    case '+':
+        Console.WriteLine(userNum1 + userNum2);
+        break;
+
+    case '-':
+        Console.WriteLine(userNum1 - userNum2);
+        break;
+    case '*':
+        Console.WriteLine(userNum1 * userNum2);
+        break;
+    case '/':
+        Console.WriteLine(userNum1 / userNum2);
+        break;
+    default:
+        Console.WriteLine("Wrong operator");
+        break;
+}
